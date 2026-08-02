2eb7a3fa4b18e9872a6fa3d87ca5d2106bf1402c
Lucas Arcoverde <larcoverde@proton.me>
2026-08-02 20:16:06 -0300

    Create system information strcut

---
+++ catfetch.c
@@ -0,0 +1,21 @@
+// print a simple fetch with a cat ascii
+// by Lucas Arcoverde de Melo, August 2nd 2026
+
+struct
+sys_info
+{
+    char user[256];
+    char hostname[256];
+    char os[256];
+    char kernel[256];
+    long up;
+    long ram_total;
+    long ram_avail;
+};
+
+int
+main(void)
+{
+    return 0;
+}
+
